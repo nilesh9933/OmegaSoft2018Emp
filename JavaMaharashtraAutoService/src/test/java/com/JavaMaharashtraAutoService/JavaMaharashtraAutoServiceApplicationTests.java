6148d356db6fb141bd2f59e7b9aec38ec2f4c139
abhishek soni <soni.abhishek324@gmail.com>
2018-01-19 09:59:06 +0530

    Java Maharashtra Auto Service

---
+++ JavaMaharashtraAutoService/src/test/java/com/JavaMaharashtraAutoService/JavaMaharashtraAutoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JavaMaharashtraAutoService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaMaharashtraAutoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
