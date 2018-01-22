ee2de053ef3a065c3edd19f2bc5e1be1756865d8
abhishek soni <soni.abhishek324@gmail.com>
2018-01-22 10:24:59 +0530

    MAS Role and Registration Page

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IA obj1 = new C();
+            obj1.GetData();
+
+            IB obj2 = new C();
+            obj2.GetData();
+
+
+            Drived abc = new Drived();
+            abc.GetData();
+            Console.ReadKey();
+        }
+    }
+
+    interface IA
+    {
+        void GetData();
+        
+    }
+    interface IB
+    {
+        void GetData();        
+    }
+
+    public class C :IA, IB
+    {
+        void IA.GetData()
+        {
+            Console.WriteLine("Interface A");
+        }
+
+        void IB.GetData()
+        {
+            Console.WriteLine("Interface B");
+        }
+    }
+
+
+    public abstract class Base
+    {
+        public virtual void GetData()
+        {
+            Console.WriteLine("I am base");
+        }
+    }
+
+    public class Drived: Base
+    {
+        public override void GetData()
+        {
+            Console.WriteLine("I am Drived");
+        }
+    }
+}
---
+++ JavaMaharashtraAutoService/src/main/java/com/JavaMaharashtraAutoService/Util/MySQLConnection.java
@@ -0,0 +1,33 @@
+package com.JavaMaharashtraAutoService.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnection {
+
+	private static Connection con = null;
+
+	public static Connection GetConnection() {
+		try {
+
+			if (con == null) {
+				String driver = "com.mysql.jdbc.Driver";
+				String URL = "jdbc:mysql://localhost:3306/MASDB";
+				String user = "root";
+				String password = "sa";
+
+				Class.forName(driver);
+				
+				con = DriverManager.getConnection(URL, user, password);
+			}
+		} 
+		
+		catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+}
