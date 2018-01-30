e43fa114a5e0b2772ad55a89e4c4221d6195ab36
abhishek soni <soni.abhishek324@gmail.com>
2018-01-30 11:17:18 +0530

    Role Assignment page using Kendo UI and GET

---
+++ JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/RolesController.java
@@ -0,0 +1,22 @@
+package com.JavaEmploymentTimes.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JavaEmploymentTimes.Dao.RoleAssignment;
+import com.JavaEmploymentTimes.Model.Roles;
+
+
+@RestController
+public class RolesController {
+
+	@RequestMapping(value="/GetUserRoles",method= RequestMethod.GET)
+	public List<Roles> GetRoles()
+	{
+		RoleAssignment rolAss=new RoleAssignment();
+		return rolAss.GetRoles();
+	}
+}
