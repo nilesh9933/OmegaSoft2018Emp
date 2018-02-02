afe48a91b388b1113251bf5f57356c178be9389a
abhishek soni <soni.abhishek324@gmail.com>
2018-02-02 10:54:03 +0530

    Whole Changes

--- JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/RolesController.java
+++ JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/RolesController.java
@@ -2,6 +2,7 @@ package com.JavaEmploymentTimes.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.JavaEmploymentTimes.Model.Roles;
 @RestController
 public class RolesController {
 
+	@CrossOrigin("*")
 	@RequestMapping(value="/GetUserRoles",method= RequestMethod.GET)
 	public List<Roles> GetRoles()
 	{
