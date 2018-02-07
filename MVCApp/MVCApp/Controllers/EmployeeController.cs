bb081eee5e1a5da9bb1febdf4a6e339b4dd7c527
abhishek soni <soni.abhishek324@gmail.com>
2018-02-07 08:28:05 +0530

    correcting project file

---
+++ MVCApp/MVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Employee(EmployeeModel emp)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCApp/MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public string empAddress { get; set; }
+    }
+}
