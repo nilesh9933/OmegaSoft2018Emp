1e3ce132bd3660c23a5582f8d85f4ee2f747259c
soniabhishek324 <soni.abhishek324@gmail.com>
2018-02-17 11:14:37 +0530

    MAS- Authentication and Authorization , View role page using Angular JS

--- JavaMaharashtraAutoService/src/main/java/com/JavaMaharashtraAutoService/Dao/RoleDao.java
+++ JavaMaharashtraAutoService/src/main/java/com/JavaMaharashtraAutoService/Dao/RoleDao.java
@@ -2,6 +2,10 @@ package com.JavaMaharashtraAutoService.Dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.JavaMaharashtraAutoService.Model.Roles;
 import com.JavaMaharashtraAutoService.Util.MySQLConnection;
@@ -47,4 +51,32 @@ public class RoleDao implements IRoleDao {
 		return "";
 	}
 
+	@Override
+	public List<Roles> GetRoles() {
+
+		List<Roles> listRoles=new ArrayList<Roles>();
+		try {
+			Statement st=con.createStatement();
+			String sql="SELECT rolId,rolName,rolDes,updateUser,updateDate\r\n" + 
+					" FROM roles where delInd='N';";
+			ResultSet rs=st.executeQuery(sql);
+			
+			while(rs.next())
+			{
+				Roles role=new Roles();
+				role.setRolId(rs.getInt("rolId"));
+				role.setRolName(rs.getString("rolName"));
+				role.setRolDes(rs.getString("rolDes"));
+				role.setUpdateUser(rs.getString("updateUser"));
+				role.setUpdateDate(rs.getDate("updateDate"));
+				
+				listRoles.add(role);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return listRoles;
+	}
 }
