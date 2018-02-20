a4e8dfafb4cfc24c25aaeba0549308494ca0f7b4
soniabhishek324 <soni.abhishek324@gmail.com>
2018-02-20 11:27:05 +0530

    Merger with master

--- JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/EpaperpostController.java
+++ JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/EpaperpostController.java
@@ -1,8 +1,11 @@
 package com.JavaEmploymentTimes.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,20 @@ import com.JavaEmploymentTimes.Model.Epaperpost;
 
 @RestController
 public class EpaperpostController {
-	@CrossOrigin("*")
 
-	@RequestMapping(value="/AddEpaperPost")
-	@ResponseBody
-	public String AddEpaperPost(@ModelAttribute ("Epaperpost") Epaperpost ePaperPost){
-		
-		EpaperPostDao obj= new EpaperPostDao();
+	@CrossOrigin("*")
+	@RequestMapping(value = "/AddEpaperPost",method=RequestMethod.POST, consumes="multipart/form-data")
+	public @ResponseBody String AddEpaperPost(@ModelAttribute("Epaperpost") Epaperpost ePaperPost) {
+
+		EpaperPostDao obj = new EpaperPostDao();
 		return obj.AddEpaperPost(ePaperPost);
-		
+
+	}
+	
+	@RequestMapping(value = "/GetEpaperDetails",method=RequestMethod.GET)
+	public List<Epaperpost> GetPaperDetails()
+	{
+		EpaperPostDao obj = new EpaperPostDao();
+		return obj.GetEPaperDetails();
 	}
 }
