72d1cb4b3f9099e8039f369b1e3247a00407e45c
soniabhishek324 <soni.abhishek324@gmail.com>
2018-02-23 10:46:49 +0530

    EMP : Sub View

--- JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/SubscriptionsController.java
+++ JavaEmploymentTimes/src/main/java/com/JavaEmploymentTimes/Controller/SubscriptionsController.java
@@ -1,5 +1,7 @@
 package com.JavaEmploymentTimes.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,13 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.JavaEmploymentTimes.Dao.SubscriptionsDao;
 import com.JavaEmploymentTimes.Model.Subscriptions;
+
 @RestController
 public class SubscriptionsController {
-	@RequestMapping(value="/AddSubscription",method=RequestMethod.POST)
+	@RequestMapping(value = "/AddSubscription", method = RequestMethod.POST)
 	@ResponseBody
-	public String AddSubscription(@ModelAttribute ("Subscriptions") Subscriptions subscriptions)
-	{
-		SubscriptionsDao obj=new SubscriptionsDao();			
+	public String AddSubscription(@ModelAttribute("Subscriptions") Subscriptions subscriptions) {
+		SubscriptionsDao obj = new SubscriptionsDao();
 		return obj.AddSubscription(subscriptions);
+	}
+
+	@RequestMapping(value = "/GetSubscriptions", method = RequestMethod.GET)
+	public @ResponseBody List<Subscriptions> GetSubscriptions() {
+
+		SubscriptionsDao obj = new SubscriptionsDao();
+		return obj.GetSubscriptions();
+
+	}
+	
+	
+	@RequestMapping(value = "/GetSubscriptionDetails", method = RequestMethod.GET)
+	public @ResponseBody Subscriptions GetSubscriptionDetails(String subId) {
+
+		SubscriptionsDao obj = new SubscriptionsDao();
+		return obj.GetSubscriptionDetails(subId);
+
 	}
 }
